e1010b0406d148d8eae1ead63ca97fab01a3317b
KSSaiTeja <saitej4865@gmail.com>
2023-08-15 13:28:43 +0530

    resolution fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logo} alt='logo' className='w-15 h-15 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             K S &nbsp;
             <span className='sm:block hidden'> Sai Teja</span>
